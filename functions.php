046d14abde252e4961e8021d4c20146a08ad0625
laulzgoay <33656556+laulzgoay@users.noreply.github.com>
2020-04-30 13:35:26 +0800

    Briefness - 1.3.0
    Briefness - 一款简约的单栏 Typecho 主题，极致简洁的风格
     1 在主题设置中增加 Gravatar头像源选择 提供第四个源为：小俊博客源
     2 🤔修改函数命名

--- functions.php
+++ functions.php
@@ -43,7 +43,8 @@ function themeConfig($form) {
     
     $GravatarUrl = new Typecho_Widget_Helper_Form_Element_Radio('GravatarUrl', 
 	array(false => _t('官方源'),
-	'https://cn.gravatar.com/avatar/' => _t('官方国内源'),
+    'https://cn.gravatar.com/avatar/' => _t('官方国内源'),
+	'https://g.xjisme.com/avatar/' => _t('小俊博客源'),
 	'https://cdn.v2ex.com/gravatar/' => _t('V2EX源')),
 	false, _t('Gravatar头像源'), _t('默认官方源'));
 	$form->addInput($GravatarUrl);
